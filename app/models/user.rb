d42207db2f5d9f07fac3c08631cfe8c904b4131e
ryu20010202 <apo200187@gmail.com>
2020-11-06 15:52:34 +0900

    ユーザー管理機能の実装、RSpecの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+  validates :email, uniqueness: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :lname, presence: true, format: { with: /[\p{katakana}ー－&&[^ -~｡-ﾟ]]+/ }
+  validates :fname, presence: true, format: { with: /[\p{katakana}ー－&&[^ -~｡-ﾟ]]+/}
+  validates :birthday, presence: true
+
 end
