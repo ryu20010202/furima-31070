04b8c2b55f0bc52036e4982e32d86bec4e535fd7
ryu20010202 <apo200187@gmail.com>
2020-11-11 15:17:17 +0900

    単体テストコードの実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,108 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '投稿の保存' do
+    context '投稿が保存できる場合' do
+      it 'image,name,text,category_id,condition_id,burden_id,area_id,day_id,priceの全てが入力されていれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '投稿が保存できない場合' do
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空では登録できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'textが空では登録できない' do
+        @item.text = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'category_idが空では登録できない'do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'category_idの内容が１では登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'condition_idが空では登録できない' do
+        @item.condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'condition_idの内容が１では登録できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+      end
+      it 'burden_idが空では登録できない' do
+        @item.burden_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden can't be blank")
+      end
+      it 'burden_idの内容が１では登録できない' do
+        @item.burden_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden must be other than 1")
+      end
+      it 'area_idが空では登録できない' do
+        @item.area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it 'area_idの内容が１では登録できない' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
+      end
+      it 'day_idが空では登録できない' do
+        @item.day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day can't be blank")
+      end
+      it 'day_idの内容が１では登録できない' do
+        @item.day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day must be other than 1")
+      end
+      it 'priceが空では登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceの内容が半角数字以外では登録できない' do
+        @item.price = 'aaaaaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceの内容が300未満の場合登録できない' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include()
+      end
+      it 'priceの内容が9999999よりも大きい場合登録できない' do
+        @item.price = 99999990
+        @item.valid?
+        expect(@item.errors.full_messages).to include()
+      end
+      it 'userが紐付いていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
