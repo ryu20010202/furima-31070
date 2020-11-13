10b8c8638117fceb87df87a7fb1b2b670f4525e6
ryu20010202 <apo200187@gmail.com>
2020-11-13 15:05:14 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_tweet, only: [:edit, :show, :create]
+  before_action :set_item, only: [:edit, :show, :create]
 
   def index
     @item = Item.all.order('created_at DESC')
